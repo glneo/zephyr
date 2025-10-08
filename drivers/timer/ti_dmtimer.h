be11db1460dfae8808a9b17d890d8b383624b903
Andrew Davis <afd@ti.com>
2025-10-08 12:27:26 -0500

    drivers: timer: ti_dmtimer: Use FIELD_PREP to remove bit shifts
    Signed-off-by: Andrew Davis <afd@ti.com>

--- drivers/timer/ti_dmtimer.h
+++ drivers/timer/ti_dmtimer.h
@@ -32,73 +32,33 @@
 #define TI_DM_TIMER_TOCR           (0x68)
 #define TI_DM_TIMER_TOWR           (0x6c)
 
-#define TI_DM_TIMER_IRQSTATUS_MAT_IT_FLAG_SHIFT                  (0)
 #define TI_DM_TIMER_IRQSTATUS_MAT_IT_FLAG_MASK                   (0x00000001)
-
-#define TI_DM_TIMER_IRQSTATUS_OVF_IT_FLAG_SHIFT                  (1)
 #define TI_DM_TIMER_IRQSTATUS_OVF_IT_FLAG_MASK                   (0x00000002)
-
-#define TI_DM_TIMER_IRQSTATUS_TCAR_IT_FLAG_SHIFT                 (2)
 #define TI_DM_TIMER_IRQSTATUS_TCAR_IT_FLAG_MASK                  (0x00000004)
 
-#define TI_DM_TIMER_IRQENABLE_SET_MAT_EN_FLAG_SHIFT              (0)
 #define TI_DM_TIMER_IRQENABLE_SET_MAT_EN_FLAG_MASK               (0x00000001)
-
-#define TI_DM_TIMER_IRQENABLE_SET_OVF_EN_FLAG_SHIFT              (1)
 #define TI_DM_TIMER_IRQENABLE_SET_OVF_EN_FLAG_MASK               (0x00000002)
-
-#define TI_DM_TIMER_IRQENABLE_SET_TCAR_EN_FLAG_SHIFT             (2)
 #define TI_DM_TIMER_IRQENABLE_SET_TCAR_EN_FLAG_MASK              (0x00000004)
-
-#define TI_DM_TIMER_IRQENABLE_CLR_MAT_EN_FLAG_SHIFT              (0)
 #define TI_DM_TIMER_IRQENABLE_CLR_MAT_EN_FLAG_MASK               (0x00000001)
-
-#define TI_DM_TIMER_IRQENABLE_CLR_OVF_EN_FLAG_SHIFT              (1)
 #define TI_DM_TIMER_IRQENABLE_CLR_OVF_EN_FLAG_MASK               (0x00000002)
-
-#define TI_DM_TIMER_IRQENABLE_CLR_TCAR_EN_FLAG_SHIFT             (2)
 #define TI_DM_TIMER_IRQENABLE_CLR_TCAR_EN_FLAG_MASK              (0x00000004)
 
-#define TI_DM_TIMER_TCLR_ST_SHIFT                                (0)
 #define TI_DM_TIMER_TCLR_ST_MASK                                 (0x00000001)
-
-#define TI_DM_TIMER_TCLR_AR_SHIFT                                (1)
 #define TI_DM_TIMER_TCLR_AR_MASK                                 (0x00000002)
-
-#define TI_DM_TIMER_TCLR_PTV_SHIFT                               (2)
 #define TI_DM_TIMER_TCLR_PTV_MASK                                (0x0000001c)
-
-#define TI_DM_TIMER_TCLR_PRE_SHIFT                               (5)
 #define TI_DM_TIMER_TCLR_PRE_MASK                                (0x00000020)
-
-#define TI_DM_TIMER_TCLR_CE_SHIFT                                (6)
 #define TI_DM_TIMER_TCLR_CE_MASK                                 (0x00000040)
-
-#define TI_DM_TIMER_TCLR_SCPWM_SHIFT                             (7)
 #define TI_DM_TIMER_TCLR_SCPWM_MASK                              (0x00000080)
-
-#define TI_DM_TIMER_TCLR_TCM_SHIFT                               (8)
 #define TI_DM_TIMER_TCLR_TCM_MASK                                (0x00000300)
-
-#define TI_DM_TIMER_TCLR_TRG_SHIFT                               (10)
 #define TI_DM_TIMER_TCLR_TRG_MASK                                (0x00000c00)
-
-#define TI_DM_TIMER_TCLR_PT_SHIFT                                (12)
 #define TI_DM_TIMER_TCLR_PT_MASK                                 (0x00001000)
-
-#define TI_DM_TIMER_TCLR_CAPT_MODE_SHIFT                         (13)
 #define TI_DM_TIMER_TCLR_CAPT_MODE_MASK                          (0x00002000)
-
-#define TI_DM_TIMER_TCLR_GPO_CFG_SHIFT                           (14)
 #define TI_DM_TIMER_TCLR_GPO_CFG_MASK                            (0x00004000)
 
-#define TI_DM_TIMER_TCRR_TIMER_COUNTER_SHIFT                     (0)
 #define TI_DM_TIMER_TCRR_TIMER_COUNTER_MASK                      (0xffffffff)
 
-#define TI_DM_TIMER_TLDR_LOAD_VALUE_SHIFT                        (0)
 #define TI_DM_TIMER_TLDR_LOAD_VALUE_MASK                         (0xffffffff)
 
-#define TI_DM_TIMER_TMAR_COMPARE_VALUE_SHIFT                     (0)
 #define TI_DM_TIMER_TMAR_COMPARE_VALUE_MASK                      (0xffffffff)
 
 #endif /* ZEPHYR_DRIVERS_TIMERS_TI_DMTIMER_H_ */
